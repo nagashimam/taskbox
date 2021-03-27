18baa2de0caec678caefdcd199ec75b90ecb4fc6
masato <masato.ngsm@gmail.com>
2021-03-27 12:45:17 +0900

    データ

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+// .storybook/main.js
+module.exports = {
+  stories: ["../src/app/components/**/*.stories.ts"],
+  addons: [
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-notes",
+  ],
+};
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+// src/app/app.module.ts
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { TaskModule } from './components/task.module';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    TaskModule,
+    NgxsModule.forRoot([]),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot(),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
